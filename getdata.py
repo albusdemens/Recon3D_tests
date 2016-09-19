de01eb376aecceef5bc6ead59667b446e9643d85
andcj <sterrior@gmail.com>
2016-09-19 14:19:54 +0200

    Now working.
    Data output is now an array of len(alpha) x len(beta) x len(omega) x
    xlen x ylen.
    An output dir is created that stores the arrays as .npy files.
    No more deprecation warnings.

--- getdata.py
+++ getdata.py
@@ -1,5 +1,10 @@
 from lib.miniged import GetEdfData
 import sys
+import time
+import os
+import warnings
+import numpy as np
+from mpi4py import MPI
 
 '''
 Inputs:
@@ -20,35 +25,70 @@ class makematrix():
 		dataname, bgpath, bgfilename,
 		poi, imgsize, outputpath, outputdir):
 
-		initminiged()
-		print self.alpha
-		print self.beta
-		print self.omega
+		self.comm = MPI.COMM_WORLD
+		self.rank = self.comm.Get_rank()
+		self.size = self.comm.Get_size()
 
-	def initminiged(self):
-		data = GetEdfData(path, filename, bg_path, bg_filename, roi, datatype)
+		imgsize = imgsize.split(',')
+		poi = poi.split(',')
+
+		if self.rank == 0:
+			start = time.time()
+			self.directory = self.makeOutputFolder(outputpath, outputdir)
+
+		roi = [
+			int(int(poi[0]) - int(imgsize[0]) / 2),
+			int(int(poi[0]) + int(imgsize[0]) / 2),
+			int(int(poi[1]) - int(imgsize[1]) / 2),
+			int(int(poi[1]) + int(imgsize[1]) / 2)]
+
+		data = GetEdfData(datadir, dataname, bgpath, bgfilename, roi)
 		self.alpha, self.beta, self.omega = data.getMetaValues()
 
-	def allFiles(self):
+		self.allFiles(data, imgsize)
+
+		if self.rank == 0:
+			stop = time.time()
+			print 'Total time: {0:8.4f} seconds.'.format(stop - start)
+
+	def makeOutputFolder(self, path, dirname):
+		directory = path + '/' + dirname
+
+		print directory
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+		return directory
+
+	def allFiles(self, data, imsiz):
 		index_list = range(len(data.meta))
+		met = data.meta
 
 		with warnings.catch_warnings():
 			warnings.simplefilter("ignore")
 			imgarray = data.makeImgArray(index_list, 50, 'linetrace')
 
-		if rank == 0:
-			np.save('/u/data/andcj/tmp/alpha.npy', self.alpha)
-			np.save('/u/data/andcj/tmp/beta.npy', self.beta)
-			np.save('/u/data/andcj/tmp/omega.npy', self.omega)
+		if self.rank == 0:
+			lena = len(self.alpha)
+			lenb = len(self.beta)
+			leno = len(self.omega)
 
-			reshapedarray = np.reshape(imgarray,[41, 181, np.shape(imgarray)[1], np.shape(imgarray)[2]])
+			bigarray = np.zeros((lena, lenb, leno, int(imsiz[0]), int(imsiz[1])))
 
-			np.save('/u/data/andcj/tmp/largetest_200x200.npy', reshapedarray)
+			for i, ind in enumerate(index_list):
+				a = np.where(self.alpha == met[ind, 0])
+				b = np.where(self.beta == met[ind, 1])
+				c = np.where(self.omega == met[ind, 2])
+
+				bigarray[a, b, c, :, :] = imgarray[ind, :, :]
+
+			np.save(self.directory + '/alpha.npy', self.alpha)
+			np.save(self.directory + '/beta.npy', self.beta)
+			np.save(self.directory + '/omega.npy', self.omega)
+
+			np.save(self.directory + '/dataarray_{}x{}.npy'.format(imsiz[0], imsiz[1]), bigarray)
 
 
 if __name__ == "__main__":
-	print sys.argv
-	print len(sys.argv)
 	if len(sys.argv) != 9:
 		print "Not enough input parameters. Data input should be:\n\
 	Directory of data\n\
