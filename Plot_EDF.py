a0f26b8242fec552cacfe4c25195bfbd0739802a
albusdemens <mannaro85@gmail.com>
2017-05-10 11:34:44 +0200

    Included image flip introduced by the lens system

--- Plot_EDF.py
+++ Plot_EDF.py
@@ -6,5 +6,17 @@ import lib.EdfFile as EF
 
 im = EF.EdfFile('/Users/Alberto/Documents/Data_analysis/DFXRM/topotomo_frelon_far_0022_0000_0002.edf')
 image = im.GetData(0)
-plt.imshow(image)
+im_flip = np.fliplr(image)
+
+fig = plt.figure()
+a=fig.add_subplot(1,2,1)
+imgplot = plt.imshow(image)
+a.set_title('Image')
+a=fig.add_subplot(1,2,2)
+imgplot = plt.imshow(im_flip)
+a.set_title('Flipped image')
+
+#plt.imshow(image)
 plt.show()
+#plt.imshow(im_flip)
+#plt.show()
