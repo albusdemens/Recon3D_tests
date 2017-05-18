a7ea9469a5b63c99a90a3adca7808cdcb14212f9
andcj <sterrior@gmail.com>
2017-05-18 10:22:56 +0200

    Added input argument functionality.

--- Plot_EDF.py
+++ Plot_EDF.py
@@ -3,8 +3,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import lib.EdfFile as EF
+import sys
 
-im = EF.EdfFile('/Users/Alberto/Documents/Data_analysis/DFXRM/topotomo_frelon_far_0022_0000_0002.edf')
+im = EF.EdfFile(sys.argv[1])
 image = im.GetData(0)
 plt.imshow(image)
 plt.show()
