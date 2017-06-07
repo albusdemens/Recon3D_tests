1fd5314db1c8ccaddc008a330be7df657b7a2d34
albusdemens <mannaro85@gmail.com>
2017-06-07 13:21:15 +0200

    Included diverging colormap

--- Plot_3D.py
+++ Plot_3D.py
@@ -1,17 +1,31 @@
+# This script selects a layer from the 3D npy array reconstructed by recon3d.py,
+# and plots the spatial distribution of the following values:
+# - chi and phi angles used to tilt the sample
+# - weight, which quantifies the quality of the fit assigning to a voxel the
+#   corresponding (chi, phi) angles
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import math
 from itertools import count
 import sys
+from cmap_map import cmap_map
 
+# Load the npy array
 A = np.load('/Users/Alberto/Documents/Data_analysis/DFXRM/Results_sunday/grain_ang_1.npy')
 
-# Find the max of the mean for the weight
+# Considering all z layers, find the max of the mean for the weight
 M = np.zeros(A.shape[2])
 for i in range(A.shape[2]):
     M[i] = np.mean(A[:,:,i,2])
 max_mean = np.max(M)
+
+Layer_max = np.zeros([A.shape[0], A.shape[1]])
+for i in range(A.shape[2]):
+    M[i] = np.mean(A[:,:,i,2])
+    if M[i] == max_mean:
+        Layer_max[:,:] = A[:,:,i,2]
 
 # Select a z value and plot the respective slice (label by label and combined labels)
 
@@ -23,32 +37,38 @@ A_z_val_2 = np.empty(shape=[A.shape[0], A.shape[1]])
 
 A_z_val_0[:,:] = A[:,:,z_val,0]#/np.mean(A[:,:,z_val,0])
 A_z_val_1[:,:] = A[:,:,z_val,1]#/np.mean(A[:,:,z_val,1])
-A_z_val_2[:,:] = A[:,:,z_val,2]/max_mean
+layer_mean = np.mean(A[:,:,z_val,2])
+for i in range(A_z_val_2[0].size):
+    for j in range(A_z_val_2[1].size):
+        A_z_val_2[i,j] = A[i,j,z_val,2]/Layer_max[i,j]
 A_z_val[:,:] = ( 0.5 * ( A_z_val_0[:,:] + A_z_val_1[:,:]))
 
+# Use an inverted colors heatmap (high values at the centre)
+inv = cmap_map(lambda x: 1-x, plt.cm.Spectral)
+
 plt.subplot(221)
-plt.imshow(A_z_val_0)
+plt.imshow(A_z_val_0, cmap = inv)
 plt.title('Theta')
 
 plt.subplot(222)
-plt.imshow(A_z_val_1)
+plt.imshow(A_z_val_1, cmap = inv)
 plt.title('Gamma')
 
 plt.subplot(223)
-plt.imshow(A_z_val_2)
+plt.imshow(A_z_val_2, cmap = inv)
 plt.title('Weight')
 
 plt.subplot(224)
-plt.imshow(A_z_val)
+plt.imshow(A_z_val-2, cmap = inv)
 plt.title('Combined value')
 
 plt.show()
 
-plt.imshow(A_z_val_2)
+sys.exit()  # This stops the script here
+
+plt.imshow(A_z_val_2, cmap = inv)
 plt.title('Weight')
 plt.show()
-
-sys.exit
 
 # Save, layer by layer, the slices of the volume
 
