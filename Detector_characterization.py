a8c16833a29cbfb64d1422b316ed44334dad2f4c
albusdemens <mannaro85@gmail.com>
2017-08-12 12:52:57 +0200

    Beam normalization succesfully implemented

---
+++ Detector_characterization.py
@@ -0,0 +1,132 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+# Load data before cleaning
+A = np.load('/u/data/alcer/DFXRM_rec/Rec_test/dataarray.npy')
+# Load data after cleaning
+B = np.load('/u/data/alcer/DFXRM_rec/Rec_test/dataarray_clean.npy')
+
+# Select the gamma, mu combination
+aa = 3
+bb = 5
+
+# For the higher intensity detector, study how the integrated intensity evolves
+# as the sample rotates
+fig = plt.figure()
+im_count = 0
+for oo in range(0, A.shape[2], 20):
+    hist_counter = np.zeros([A.shape[4], 3])
+    for k in range(A.shape[4]):
+        hist_counter[k,0] = k
+        hist_counter[k, 1] = sum(A[aa,bb,oo,:,k])
+        hist_counter[k, 2] = sum(B[aa,bb,oo,:,k])
+    plt.subplot(1,2,1)
+    plt.ylim(9E4, 1E5)
+    plt.grid(True)
+    plt.plot(hist_counter[:,0], hist_counter[:,1], 'o', markersize=1.5)
+    plt.title('Raw data')
+    plt.xlabel('Y pixel')
+    plt.ylabel('Summed intensity')
+    plt.subplot(1,2,2)
+    plt.ylim(5E3, 8E3)
+    plt.grid(True)
+    plt.plot(hist_counter[:,0], hist_counter[:,2], 'o', markersize=1.5)
+    plt.title('Cleaned data')
+    plt.xlabel('Y pixel')
+    plt.ylabel('Summed intensity')
+plt.show()
+
+fig = plt.figure()
+im_count = 0
+for oo in range(0, A.shape[2], 20):
+    hist_counter = np.zeros([A.shape[4], 3])
+    for k in range(A.shape[3]):
+        hist_counter[k,0] = k
+        hist_counter[k, 1] = sum(A[aa,bb,oo,k,:])
+        hist_counter[k, 2] = sum(B[aa,bb,oo,:,k])
+    plt.subplot(1,2,1)
+    plt.ylim(9E4, 1E5)
+    plt.plot(hist_counter[:,0], hist_counter[:,1], 'o', markersize=1.5)
+    plt.title('Summed intensity along X')
+    plt.xlabel('X pixel')
+    plt.ylabel('Summed intensity')
+    plt.subplot(1,2,2)
+    plt.ylim(5E3, 8E3)
+    plt.plot(hist_counter[:,0], hist_counter[:,2], 'o', markersize=1.5)
+    plt.title('Cleaned data')
+    plt.xlabel('X pixel')
+    plt.ylabel('Summed intensity')
+plt.show()
+
+### Look at the distribution on the integrated intensity along Y
+omega = 5
+
+# Plot intensity sum along Y for the raw data
+fig = plt.figure()
+plt.title('Summed intensity along Y, raw data, one proj')
+im_count = 0
+for i in range(A.shape[0]):
+    for j in range(A.shape[1]):
+        hist_counter = np.zeros([A.shape[4], 2])
+        im_count = im_count + 1
+        for k in range(A.shape[4]):
+            hist_counter[k,0] = k
+            hist_counter[k, 1] = sum(A[i,j,omega,:,k])
+
+        plt.subplot(A.shape[0], A.shape[1], im_count)
+        plt.scatter(hist_counter[:,0], hist_counter[:,1], s=1)
+
+plt.show()
+
+# Plot intensity sum along Y for the cleaned data
+fig = plt.figure()
+plt.title('Summed intensity along Y, cleaned data, one proj')
+im_count = 0
+for i in range(A.shape[0]):
+    for j in range(A.shape[1]):
+        hist_counter = np.zeros([A.shape[4], 2])
+        im_count = im_count + 1
+        for k in range(A.shape[4]):
+            hist_counter[k,0] = k
+            if sum(B[i,j,omega,:,k]) < 5000:
+                hist_counter[k, 1] = sum(B[i,j,omega,:,k])
+
+        plt.subplot(A.shape[0], A.shape[1], im_count)
+        plt.scatter(hist_counter[:,0], hist_counter[:,1], s=1)
+
+plt.show()
+
+# Plot intensity sum along Y for the raw data. All projections considered
+fig = plt.figure()
+plt.title('Summed intensity along Y, raw data, all proj')
+im_count = 0
+for i in range(A.shape[0]):
+    for j in range(A.shape[1]):
+        hist_counter = np.zeros([A.shape[4], 2])
+        im_count = im_count + 1
+        for k in range(A.shape[4]):
+            hist_counter[k,0] = k
+            hist_counter[k, 1] = np.sum(A[i,j,:,:,k])
+
+        plt.subplot(A.shape[0], A.shape[1], im_count)
+        plt.scatter(hist_counter[:,0], hist_counter[:,1], s=1)
+
+plt.show()
+
+# Plot intensity sum along Y for the cleaned data. All projections considered
+fig = plt.figure()
+plt.title('Summed intensity along Y, cleaned data, all proj')
+im_count = 0
+for i in range(A.shape[0]):
+    for j in range(A.shape[1]):
+        hist_counter = np.zeros([A.shape[4], 2])
+        im_count = im_count + 1
+        for k in range(A.shape[4]):
+            hist_counter[k,0] = k
+            hist_counter[k, 1] = np.sum(B[i,j,:,:,k])
+
+        plt.subplot(A.shape[0], A.shape[1], im_count)
+        plt.scatter(hist_counter[:,0], hist_counter[:,1], s=1)
+
+plt.show()
--- Morph_operations.py
+++ Morph_operations.py
@@ -1,22 +1,84 @@
+# Testing script for morphology operations, to be incorporated in getdata.py
+
 import numpy as np
 import matplotlib.pyplot as plt
 import mahotas as mh
+import sys
 
-A = np.load('/u/data/alcer/DFXRM_rec/Rec_test/dataarray_clean_norm.npy')
+A = np.load('/u/data/alcer/DFXRM_rec/Rec_test/dataarray_clean.npy')
 
 B = np.zeros([A.shape[0], A.shape[1], A.shape[2], A.shape[3], A.shape[4],])
 
-for i in range(A.shape[0]):
-    for j in range(A.shape[1]):
-        for k in range(A.shape[2]):
-            IM = np.zeros([A.shape[3], A.shape[4]])
-            IM[:,:] = A[i,j,k,:,:]
-            IM[IM>80] = 1
-            B[i,j,k,:,:] = IM[:,:]
+mean_proj = np.zeros([A.shape[2], 3])
+for ii in range(A.shape[2]):
+    mean_proj[ii,0] = ii
+    mean_proj[ii,1] = np.mean(A[:,:,ii,:,:])
+    #mean_proj[ii,2] = np.sum(A[:,:,ii,:,:])
+    B[:,:,ii,:,:] = A[:,:,ii,:,:] / mean_proj[ii,1]
+    mean_proj[ii,2] = np.sum(B[:,:,ii,:,:])
+mean_max = max(mean_proj[:,1])
 
 fig = plt.figure()
 plt.subplot(1,2,1)
-plt.imshow(A[3,3,1,:,:])
+plt.scatter(mean_proj[:,0], mean_proj[:,1])
 plt.subplot(1,2,2)
-plt.imshow(B[3,3,1,:,:])
+plt.scatter(mean_proj[:,0], mean_proj[:,2] * mean_max)
+plt.show()
+sys.exit()
+
+thr = np.zeros([A.shape[2], 2])
+for k in range(0,A.shape[2],10):
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            IM = np.zeros([A.shape[3], A.shape[4]])
+            IM[:,:] = A[i,j,k,:,:]
+            thr[k,0] = k
+            # The threshold value adapts to the intensity 
+            # Recorded for a given projection
+	    thr[k,1] = 40 * mean_proj[k,1] / mean_max
+
+	    IM[IM < thr[k,1]] = 0
+            IM[IM >= thr[k,1]] = 1
+            B[i,j,k,:,:] = IM[:,:]
+            
+            print int(thr[k,1]), i, j, k
+            fig = plt.figure()
+            plt.subplot(1,2,1)
+            plt.imshow(A[i,j,k,:,:])
+            plt.subplot(1,2,2)
+            plt.imshow(B[i,j,k,:,:])
+            plt.show()
+
+I_int_1 = np.zeros([A.shape[2], 3])
+I_int_2 = np.zeros([A.shape[2], 3])
+for ii in range(A.shape[2]):
+    I_int_1[ii,0] = ii
+    I_int_2[ii,0] = ii
+    I_int_1[ii,1] = np.sum(A[:,:,ii,:,:])
+    I_int_2[ii,1] = np.sum(B[:,:,ii,:,:])
+    I_int_1[ii,2] = np.mean(A[:,:,ii,:,:])
+    I_int_2[ii,2] = np.mean(B[:,:,ii,:,:])
+
+fig = plt.figure()
+plt.subplot(1,2,1)
+plt.imshow(A[4,6,138,:,:])
+plt.subplot(1,2,2)
+plt.imshow(B[4,6,138,:,:])
+plt.show()
+
+sys.exit()
+
+fig = plt.figure()
+plt.subplot(2,2,1)
+plt.scatter(I_int_1[:,0], I_int_1[:,1])
+plt.title('Sum before binarization')
+plt.subplot(2,2,2)
+plt.scatter(I_int_2[:,0], I_int_2[:,1])
+plt.title('Sum after binarization')
+plt.subplot(2,2,3)
+plt.scatter(I_int_1[:,0], I_int_1[:,2])
+plt.title('Mean before binarization')
+plt.subplot(2,2,4)
+plt.scatter(I_int_2[:,0], I_int_2[:,2])
+plt.title('Mean after binarization')
 plt.show()
